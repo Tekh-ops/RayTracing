ce463631c2583c4a06fc24f352e2af6203bd262d
Tekh-ops <jugger.nut2234@gmail.com>
2021-09-16 16:39:31 +0530

    Build the project files

---
+++ build/CMakeFiles/RayTracing.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/mac/temp/RayTracing/src/lights.cpp" "CMakeFiles/RayTracing.dir/src/lights.cpp.o" "gcc" "CMakeFiles/RayTracing.dir/src/lights.cpp.o.d"
+  "/Users/mac/temp/RayTracing/src/main.cpp" "CMakeFiles/RayTracing.dir/src/main.cpp.o" "gcc" "CMakeFiles/RayTracing.dir/src/main.cpp.o.d"
+  "/Users/mac/temp/RayTracing/src/objects.cpp" "CMakeFiles/RayTracing.dir/src/objects.cpp.o" "gcc" "CMakeFiles/RayTracing.dir/src/objects.cpp.o.d"
+  "/Users/mac/temp/RayTracing/src/scene.cpp" "CMakeFiles/RayTracing.dir/src/scene.cpp.o" "gcc" "CMakeFiles/RayTracing.dir/src/scene.cpp.o.d"
+  "/Users/mac/temp/RayTracing/src/util.cpp" "CMakeFiles/RayTracing.dir/src/util.cpp.o" "gcc" "CMakeFiles/RayTracing.dir/src/util.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/RayTracing.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "../bin/RayTracing"
+  "../bin/RayTracing.pdb"
+  "CMakeFiles/RayTracing.dir/src/lights.cpp.o"
+  "CMakeFiles/RayTracing.dir/src/lights.cpp.o.d"
+  "CMakeFiles/RayTracing.dir/src/main.cpp.o"
+  "CMakeFiles/RayTracing.dir/src/main.cpp.o.d"
+  "CMakeFiles/RayTracing.dir/src/objects.cpp.o"
+  "CMakeFiles/RayTracing.dir/src/objects.cpp.o.d"
+  "CMakeFiles/RayTracing.dir/src/scene.cpp.o"
+  "CMakeFiles/RayTracing.dir/src/scene.cpp.o.d"
+  "CMakeFiles/RayTracing.dir/src/util.cpp.o"
+  "CMakeFiles/RayTracing.dir/src/util.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/RayTracing.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
